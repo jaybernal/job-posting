f93fba2223234011448a59cf2a1d193fb00da36e
Jay Bernal <jjbernal@gmail.com>
2016-05-11 10:48:58 -0500

    added user model

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    if @user = login(params[:email], params[:password])
+      if @user
+        redirect_back_or_to users_path, notice: "Logged in Successfully"
+      else
+        flash.now[:alert] = "Login Failed"
+        render :new
+      end
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to users_path, notice: "Logged Out!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  root "users#index"
+
+  resources :users, only: [:new, :show, :index, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "/login" => "sessions#new", :as => :login
+  get "/logout" => 'sessions#destroy', :as => :logout
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
