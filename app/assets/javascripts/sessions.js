84951f52d817ace71550f7057a27ecc72430007a
Jay Bernal <jjbernal@gmail.com>
2016-05-24 15:38:10 -0500

    added js to all login buttoms

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,19 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 window.addEventListener("load", setUpPage);{
-
-function changeID(){
   var change = document.getElementById("showLogin")
-  for(var i = 0; i<elements.length; i++){
-    change[i].addEventListener("click", showLogin)
-    }
+    change.addEventListener("click", showLogin());
   }
-}
 
 function showLogin(){
+  document.getElementById("showLogin").className = "sign-page-hidden";
   document.getElementById("signInPage").className = "sign-page-appear";
-  document.getElementById("signInButton").className = "sign-page-hidden";
-
 }
 
 
